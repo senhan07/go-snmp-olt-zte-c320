f9c137a94b148724975d41b52548b29631558199
sumitroajiprabowo <bowo@anakdesa.id>
2025-04-13 15:52:20 +0700

    refactor: validate SNMP config and remove unused global logger

--- pkg/snmp/snmp.go
+++ pkg/snmp/snmp.go
@@ -11,15 +11,17 @@ import (
 )
 
 var (
-	snmpHost      string
-	snmpPort      uint16
-	snmpCommunity string
-	logSnmp       gosnmp.Logger
+	snmpHost      string // SNMP host
+	snmpPort      uint16 // SNMP port
+	snmpCommunity string // SNMP community
+	//logSnmp       gosnmp.Logger // Logger for SNMP
 )
 
 // SetupSnmpConnection is a function to set up snmp connection
 func SetupSnmpConnection(config *config.Config) (*gosnmp.GoSNMP, error) {
+	var logSnmp gosnmp.Logger
 
+	// Check if the application is running in development or production environment
 	if os.Getenv("APP_ENV") == "development" || os.Getenv("APP_ENV") == "production" {
 		snmpHost = os.Getenv("SNMP_HOST")
 		snmpPort = utils.ConvertStringToUint16(os.Getenv("SNMP_PORT"))
@@ -32,19 +34,26 @@ func SetupSnmpConnection(config *config.Config) (*gosnmp.GoSNMP, error) {
 		logSnmp = gosnmp.NewLogger(log.New(os.Stdout, "", 0))
 	}
 
+	// Check if SNMP configuration is valid
+	if snmpHost == "" || snmpPort == 0 || snmpCommunity == "" {
+		return nil, fmt.Errorf("konfigurasi SNMP tidak valid")
+	}
+
+	// Create a new SNMP target instance
 	target := &gosnmp.GoSNMP{
 		Target:    snmpHost,
 		Port:      snmpPort,
 		Community: snmpCommunity,
 		Version:   gosnmp.Version2c,
 		Timeout:   time.Duration(30) * time.Second,
-		//Retries:   3
-		Logger: logSnmp,
+		Retries:   3,
+		Logger:    logSnmp,
 	}
 
+	// Connect to the SNMP target
 	err := target.Connect()
 	if err != nil {
-		return nil, fmt.Errorf("gagal terhubung: %w", err)
+		return nil, fmt.Errorf("gagal terhubung ke SNMP: %w", err)
 	}
 
 	return target, nil
